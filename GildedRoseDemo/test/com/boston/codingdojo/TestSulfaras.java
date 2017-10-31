6005bdebbb693d696f7edcf319352cca725b5992
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2017-10-30 20:51:48 -0400

    Moved testDaysToExpire and testQuality to util method

--- GildedRoseDemo/test/com/boston/codingdojo/TestSulfaras.java
+++ GildedRoseDemo/test/com/boston/codingdojo/TestSulfaras.java
@@ -9,43 +9,30 @@ public class TestSulfaras {
 	@Test
 	public void testQualityDoesNotChange() {
 		// last three params are sellIn, quality, expected quality
-		testQuality(GildedRose.ITEM_SULFARAS, 5, 80, 80);
+		TestGildedRoseUtil.testQuality(GildedRose.ITEM_SULFARAS, 5, 80, 80);
 	}
 
 	@Test
 	public void testErrorQualityNot50() {
 		// last three params are sellIn, quality, expected quality
-		testQuality(GildedRose.ITEM_SULFARAS, 8, 55, 55);
-		testQuality(GildedRose.ITEM_SULFARAS, 4, 45, 45);
+		TestGildedRoseUtil.testQuality(GildedRose.ITEM_SULFARAS, 8, 55, 55);
+		TestGildedRoseUtil.testQuality(GildedRose.ITEM_SULFARAS, 4, 45, 45);
 	}
 
 	@Test
 	public void testErrorQualityZeroOrNegative() {
 		// last three params are sellIn, quality, expected quality
-		testQuality(GildedRose.ITEM_SULFARAS, 7, 0, 0);
-		testQuality(GildedRose.ITEM_SULFARAS, 2, 45, 45);
+		TestGildedRoseUtil.testQuality(GildedRose.ITEM_SULFARAS, 7, 0, 0);
+		TestGildedRoseUtil.testQuality(GildedRose.ITEM_SULFARAS, 2, 45, 45);
 	}
 
 	@Test
 	public void testDaysToExpireDoesNotChange() {
 		// last three params are sellIn, quality, expected quality
-		testDaysToExpire(GildedRose.ITEM_SULFARAS, 7, 80, 7);
-		testDaysToExpire(GildedRose.ITEM_SULFARAS, 0, 80, 0);
-		testDaysToExpire(GildedRose.ITEM_SULFARAS, -3, 80, -3);
-		testDaysToExpire(GildedRose.ITEM_SULFARAS, 4, 45, 4);
+		TestGildedRoseUtil.testDaysToExpire(GildedRose.ITEM_SULFARAS, 7, 80, 7);
+		TestGildedRoseUtil.testDaysToExpire(GildedRose.ITEM_SULFARAS, 0, 80, 0);
+		TestGildedRoseUtil.testDaysToExpire(GildedRose.ITEM_SULFARAS, -3, 80, -3);
+		TestGildedRoseUtil.testDaysToExpire(GildedRose.ITEM_SULFARAS, 4, 45, 4);
 	}
 
-	public void testQuality(String itemName, int daysToExpire, int quality, int expectedQuality) {
-		Item[] items = {new Item(itemName,daysToExpire,quality)};
-		GildedRose gildedRose = new GildedRose(items);
-		gildedRose.updateQuality();
-		assertEquals (expectedQuality, gildedRose.items[0].quality);
-	}
-
-	public void testDaysToExpire(String itemName, int daysToExpire, int quality, int expectedDaysToExpire) {
-		Item[] items = {new Item(itemName,daysToExpire,quality)};
-		GildedRose gildedRose = new GildedRose(items);
-		gildedRose.updateQuality();
-		assertEquals (expectedDaysToExpire, gildedRose.items[0].sellIn);
-	}
 }
