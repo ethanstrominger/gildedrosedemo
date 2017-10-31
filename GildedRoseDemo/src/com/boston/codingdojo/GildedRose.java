0f0f2903421d8ffb5bf098f7f2fda2e8513de01e
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2017-10-31 18:10:14 -0400

    Moved tests based on item name to well named methods for clarity

--- GildedRoseDemo/src/com/boston/codingdojo/GildedRose.java
+++ GildedRoseDemo/src/com/boston/codingdojo/GildedRose.java
@@ -12,54 +12,95 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(ITEM_BRIE)
-                    && !items[i].name.equals(ITEM_PASS)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(ITEM_SULFARAS)) {
-                        items[i].quality = items[i].quality - 1;
+            Item item = items[i];
+			if (itemIsNotBrieOrPass(item)) {
+                if (item.quality > 0) {
+                    if (itemIsNotSulfaras(item)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(ITEM_PASS)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (itemIsPass(item)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(ITEM_SULFARAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (itemIsNotSulfaras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(ITEM_BRIE)) {
-                    if (!items[i].name.equals(ITEM_PASS)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(ITEM_SULFARAS)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (itemIsNotBrie(item)) {
+                    if (itemIsNotPass(item)) {
+                        if (item.quality > 0) {
+                            if (itemIsNotSulfaras(item)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
     }
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	public boolean itemIsNotPass(Item item) {
+		return !itemIsPass(item);
+	}
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	public boolean itemIsNotBrie(Item item) {
+		return !item.name.equals(ITEM_BRIE);
+	}
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	public boolean itemIsPass(Item item) {
+		return item.name.equals(ITEM_PASS);
+	}
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	public boolean itemIsNotSulfaras(Item item) {
+		return !item.name.equals(ITEM_SULFARAS);
+	}
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	public boolean itemIsNotBrieOrPass(Item item) {
+		return itemIsNotBrie(item)
+		        && itemIsNotPass(item);
+	}
 }
