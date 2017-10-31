0b1f93917a7cf25cb884d270b7078028c109d0f2
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2017-10-30 20:35:58 -0400

    Finish Sulfaras tests

--- GildedRoseDemo/test/com/boston/codingdojo/TestSulfaras.java
+++ GildedRoseDemo/test/com/boston/codingdojo/TestSulfaras.java
@@ -26,6 +26,15 @@ public class TestSulfaras {
 		testQuality(GildedRose.ITEM_SULFARAS, 2, 45, 45);
 	}
 
+	@Test
+	public void testDaysToExpireDoesNotChange() {
+		// last three params are sellIn, quality, expected quality
+		testDaysToExpire(GildedRose.ITEM_SULFARAS, 7, 80, 7);
+		testDaysToExpire(GildedRose.ITEM_SULFARAS, 0, 80, 0);
+		testDaysToExpire(GildedRose.ITEM_SULFARAS, -3, 80, -3);
+		testDaysToExpire(GildedRose.ITEM_SULFARAS, 4, 45, 4);
+	}
+
 	public void testQuality(String itemName, int daysToExpire, int quality, int expectedQuality) {
 		Item[] items = {new Item(itemName,daysToExpire,quality)};
 		GildedRose gildedRose = new GildedRose(items);
@@ -33,4 +42,10 @@ public class TestSulfaras {
 		assertEquals (expectedQuality, gildedRose.items[0].quality);
 	}
 
+	public void testDaysToExpire(String itemName, int daysToExpire, int quality, int expectedDaysToExpire) {
+		Item[] items = {new Item(itemName,daysToExpire,quality)};
+		GildedRose gildedRose = new GildedRose(items);
+		gildedRose.updateQuality();
+		assertEquals (expectedDaysToExpire, gildedRose.items[0].sellIn);
+	}
 }
